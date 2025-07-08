f7d5e7929707972bd1470c7941b453374ca29500
Giraud Sasha <giraudsasha@gmail.com>
2025-07-08 14:50:56 +0200

    fixed last details

--- src/Controller/TrafficLightDetailController.php
+++ src/Controller/TrafficLightDetailController.php
@@ -5,12 +5,14 @@ namespace App\Controller;
 use App\Entity\TrafficLight;
 use App\Entity\Comment;
 use App\Form\CommentType;
+use App\Form\CommentEditType;
 use App\Repository\CommentRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class TrafficLightDetailController extends AbstractController
 {
@@ -42,5 +44,57 @@ class TrafficLightDetailController extends AbstractController
             'comments' => $comments,
             'commentForm' => $form->createView(),
         ]);
+    }
+
+    #[Route('/comment/{id}/edit', name: 'comment_edit')]
+    #[IsGranted('ROLE_USER')]
+    public function editComment(
+        Comment $comment,
+        Request $request,
+        EntityManagerInterface $em
+    ): Response {
+        if ($comment->getUser() !== $this->getUser() && !$this->isGranted('ROLE_ADMIN')) {
+            throw $this->createAccessDeniedException('Vous ne pouvez pas modifier ce commentaire.');
+        }
+
+        $form = $this->createForm(CommentEditType::class, $comment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setUpdatedAt(new \DateTimeImmutable());
+            $em->flush();
+
+            $this->addFlash('success', 'Commentaire modifié !');
+            return $this->redirectToRoute('traffic_light_detail', ['id' => $comment->getTrafficLight()->getId()]);
+        }
+
+        return $this->render('comment/edit.html.twig', [
+            'commentForm' => $form->createView(),
+            'comment' => $comment,
+        ]);
+    }
+
+    #[Route('/comment/{id}/delete', name: 'comment_delete', methods: ['POST'])]
+    #[IsGranted('ROLE_USER')]
+    public function deleteComment(
+        Comment $comment,
+        Request $request,
+        EntityManagerInterface $em
+    ): Response {
+        if ($comment->getUser() !== $this->getUser() && !$this->isGranted('ROLE_ADMIN')) {
+            throw $this->createAccessDeniedException('Vous ne pouvez pas supprimer ce commentaire.');
+        }
+
+        if ($this->isCsrfTokenValid('delete'.$comment->getId(), $request->request->get('_token'))) {
+            $trafficLightId = $comment->getTrafficLight()->getId();
+            $em->remove($comment);
+            $em->flush();
+
+            $this->addFlash('success', 'Commentaire supprimé !');
+            return $this->redirectToRoute('traffic_light_detail', ['id' => $trafficLightId]);
+        }
+
+        $this->addFlash('error', 'Erreur lors de la suppression.');
+        return $this->redirectToRoute('traffic_light_detail', ['id' => $comment->getTrafficLight()->getId()]);
     }
 }
--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\CommentRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CommentRepository::class)]
@@ -13,56 +14,80 @@ class Comment
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(type: "text")]
+    #[ORM\Column(type: Types::TEXT)]
     private ?string $content = null;
 
     #[ORM\Column]
     private ?\DateTimeImmutable $createdAt = null;
 
-    #[ORM\ManyToOne(targetEntity: User::class)]
-    private ?User $user = null;
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $updatedAt = null;
 
-    #[ORM\ManyToOne(targetEntity: TrafficLight::class)]
+    #[ORM\ManyToOne(inversedBy: 'comments')]
+    #[ORM\JoinColumn(nullable: false)]
     private ?TrafficLight $trafficLight = null;
+
+    #[ORM\ManyToOne(inversedBy: 'comments')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user = null;
 
     public function getId(): ?int
     {
         return $this->id;
     }
+
     public function getContent(): ?string
     {
         return $this->content;
     }
-    public function setContent(string $content): self
+
+    public function setContent(string $content): static
     {
         $this->content = $content;
         return $this;
     }
+
     public function getCreatedAt(): ?\DateTimeImmutable
     {
         return $this->createdAt;
     }
-    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
     {
         $this->createdAt = $createdAt;
         return $this;
     }
-    public function getUser(): ?User
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
     {
-        return $this->user;
+        return $this->updatedAt;
     }
-    public function setUser(?User $user): self
+
+    public function setUpdatedAt(?\DateTimeImmutable $updatedAt): static
     {
-        $this->user = $user;
+        $this->updatedAt = $updatedAt;
         return $this;
     }
+
     public function getTrafficLight(): ?TrafficLight
     {
         return $this->trafficLight;
     }
-    public function setTrafficLight(?TrafficLight $trafficLight): self
+
+    public function setTrafficLight(?TrafficLight $trafficLight): static
     {
         $this->trafficLight = $trafficLight;
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
         return $this;
     }
 }
